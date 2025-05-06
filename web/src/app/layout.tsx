31eb4ac620258c1cf06b6189be222f82c95a92c9
Freya Adgey <freya.adgey@instil.co>
2025-05-06 13:33:31 +0100

    feat:adding recipe page

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Chopin</header>
+        <nav>
+          <button><Link href="/">Go Home</Link></button>
+          <button><Link href="/recipes">Recipes</Link></button>
+        </nav>
+
         {children}
       </body>
     </html>
---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Homepage</h1>
+    </div>
+  );
+}
